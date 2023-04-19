9e81c37b9a3865c82a8b1dd07ad103418faa681e
Brian Jemilo II <jemiloii@gmail.com>
2023-04-19 15:13:26 -0500

    Update Year and add LLC

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -4,10 +4,21 @@
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-12 col-md-12 col-sm-6">
-                        <p>Copyright &copy; 2020 Convention Foam Fighting. All Rights Reserved</p>
+                        <p>Copyright &copy; {{ year }} Convention Foam Fighting, LLC. All Rights Reserved</p>
                     </div>
                 </div>
             </div>
         </div>
     </footer>
 </template>
+
+<script>
+  export default {
+    name: "Footer",
+    computed: {
+      year() {
+        return new Date().getFullYear();
+      }
+    }
+  };
+</script>
